5b83faed7bb8a986583fd376cd9669e7b30fa4d6
Sudip-5472 <23f3004246@ds.study.iitm.ac.in>
2026-01-20 21:25:45 +0530

    Update generate_numbers.py

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,35 @@
 def natural_numbers(n):
-    #Returns the list of natural numbers
-    #Code here
+    # Returns the list of natural numbers from 1 to n
+    return list(range(1, n + 1))
 
 
 def nth_fibonacci(n):
-    #Returns n-th fibonacci number
-    #Code here
+    # Returns the n-th Fibonacci number
+    if n <= 0:
+        return None
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(3, n + 1):
+        a, b = b, a + b
+
+    return b
+
 
 def is_prime(n):
-    #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    # Return True if 'n' is prime, otherwise returns False
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+
+    return True
