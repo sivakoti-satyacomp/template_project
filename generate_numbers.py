265bf94031547bbd29f16e2ea960921c3072f780
GAHANA THAKUR <25f2007841@ds.study.iitm.ac.in>
2026-01-20 21:32:38 +0530

    Updated the program from 25f2007841@ds.study.iitm.ac.in
    Programs Modified

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,33 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
-    #Code here
+  n = int(input("Enter a number: "))
+
+for i in range(1, n + 1):
+    print(i)
+
 
 
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
-    #Code here
+   n = int(input("Enter number of terms: "))
+
+a, b = 0, 1
+
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
+
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not a prime number")
+else:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
