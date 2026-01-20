53f3e14aab4b7b54800629ee87b49df23d5b43c8
basith <23f3002766@ds.study.iitm.ac.in>
2026-01-20 23:24:25 +0530

    feature: Added the function to calculate nth_fibonacci

--- generate_numbers.py
+++ generate_numbers.py
@@ -6,6 +6,18 @@ def natural_numbers(n):
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
     #Code here
+    if n <= 0:
+        return None
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(3, n + 1):
+        a, b = b, a + b
+
+    return b
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
