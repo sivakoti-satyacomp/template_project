00d0776044dc2d8fe8a0e9a92d0908b9e109de98
Shruvil Shrivastava <shruvil30july2@gmail.com>
2026-01-21 00:00:39 +0530

    Simplify docstrings in generate_numbers.py
    Removed unnecessary docstring formatting and simplified the comments.

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,15 +1,10 @@
 def natural_numbers(n):
-    """
-    Returns a list of first n natural numbers
-
-    """
+    "Returns a list of first n natural numbers"
 
     return list(range(1,n+1))
 
 def nth_fibonacci(n):
-    """
-    Returns the nth Fibonacci number
-    """
+    "Returns the nth Fibonacci number"
     if n<=0:
         return None
     if n==1:
@@ -23,9 +18,7 @@ def nth_fibonacci(n):
     return b
 
 def is_prime(n):
-    """
-    Returns True if n is a prime number, else False
-    """
+    "Returns True if n is a prime number, else False"
     if n<=1:
         return False
 
