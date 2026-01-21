3c01c71b4cdb0be64ca4663f12d1ee43cc3b5eba
25f2003203 <25f2003203@ds.study.iitm.ac.in>
2026-01-21 13:09:47 +0530

    Adding generate_numbers.py from branch functions_feature

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,15 @@
 def natural_numbers(n):
-    #Returns the list of natural numbers
-    #Code here
-
+    return [x for x in range(1,n+1)]
 
 def nth_fibonacci(n):
-    #Returns n-th fibonacci number
-    #Code here
+    a,b = 0,1
+    for i in range(n):
+        a,b = b,a+b
+    return a
 
 def is_prime(n):
-    #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+
