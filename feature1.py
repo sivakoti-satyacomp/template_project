2ab0fb2ef98a8af3244502dc22c5763b8f961820
25f2007910 <25f2007910@ds.study.iitm.ac.in>
2026-01-21 09:58:15 +0530

    All 3 Features are added

---
+++ feature1.py
@@ -0,0 +1,19 @@
+def natural_numbers(n):
+    """Generate a list of the first n natural numbers."""
+    return list(range(1, n + 1))
+
+def nth_fibonacci(n):
+    """Return the nth Fibonacci number."""
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+def is_prime(num):
+    """Check if a number is prime."""
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
