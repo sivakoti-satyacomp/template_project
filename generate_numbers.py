49ca2f934088082d46006ec3c3b17076120ee789
Manish Kumar <25f2000136@ds.study.iitm.ac.in>
2026-01-21 19:35:12 +0530

    Add functions for natural numbers, Fibonacci, and prime check
    Implemented functions to generate natural numbers, compute the n-th Fibonacci number, and check for prime numbers.

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,34 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
-    #Code here
-
+    return list(range(1, n + 1))
 
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
-    #Code here
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    prev, curr = 0, 1
+    for _ in range(2, n + 1):
+        prev, curr = curr, prev + curr
+    
+    return curr
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    
+    # Check odd divisors up to sqrt(n)
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    
+    return True
