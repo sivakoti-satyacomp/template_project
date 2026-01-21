718051500f844ef29c6b75988a816fdc7c815562
22f2001565 <22f2001565@ds.study.iitm.ac.in>
2026-01-21 18:34:00 +0530

    Functions Implemented

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,30 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
-    #Code here
-
+    numbers= []
+    for i in range(1,n+1):
+        numbers.append(i)
+    return numbers
 
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
-    #Code here
-
+    a= 0
+    b= 1
+    if n==1:
+        return a
+    elif n==2:
+        return b
+    else:
+        for i in range(2, n):
+            c= a+b
+            a=b
+            b=c
+        return c
+    
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    if n<=1:
+        return False
+    for i in range(2, n):
+        if n%i == 0:
+            return False
+    return True
