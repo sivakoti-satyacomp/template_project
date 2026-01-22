f55ed3c9561b436bd24deca886b3b5a90d7bdad7
23f3003645 <23f3003645@ds.study.iitm.ac.in>
2026-01-22 19:46:57 +0530

    Implemented natural numbers, fibonacci and prime functions

---
+++ main.py
@@ -0,0 +1,25 @@
+def natural_numbers(n):
+    return list(range(1, n + 1))
+
+
+def nth_fibonacci(n):
+    if n <= 0:
+        return None
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(3, n + 1):
+        a, b = b, a + b
+    return b
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
