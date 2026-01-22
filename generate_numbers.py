5d5022f246c0bc9dd359f85df5805f37127510aa
23f2000592 <23f2000592@ds.study.iitm.ac.in>
2026-01-22 12:32:24 +0530

    Add is_prime method

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,20 @@
 def natural_numbers(n):
-    #Returns the list of natural numbers
-    #Code here
+    # Returns the list of natural numbers
+    pass
 
 
 def nth_fibonacci(n):
-    #Returns n-th fibonacci number
-    #Code here
+    # Returns n-th fibonacci number
+    pass
+
 
 def is_prime(n):
-    #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    # Return true if 'n' is prime, otherwise returns false
+    if n == 1:
+        return False
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+
+    return True
