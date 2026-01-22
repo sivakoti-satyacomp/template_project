f835dc2d6a52796b1016c1b2e071127f14588ec6
Preethy <25f2007872@ds.study.iitm.ac.in>
2026-01-21 16:27:21 -0800

    added the three functions to generate_numbers.py

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,27 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
-    #Code here
-
-
+    numlist = []
+    for i in range(1, n+1):
+        numlist.append(i)
+    return numlist    
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
-    #Code here
+    if n <= 1:
+        return n
+    return nth_fibonacci(n - 1) + nth_fibonacci(n - 2)
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2  
+    return True
+
