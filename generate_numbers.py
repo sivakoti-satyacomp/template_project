f145328a2ad2e7928204b6144ef5dad79bc65fc6
24f2005237 <24f2005237@ds.study.iitm.ac.ingit config --global user.email 24f2005237@ds.study.iitm.ac.ingit commit -m Milestone-0>
2026-01-22 13:32:39 +0000

    Implemented three functions

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,17 @@
 def natural_numbers(n):
-    #Returns the list of natural numbers
-    #Code here
+    return [i for i in range(1, n + 1)]
 
 
 def nth_fibonacci(n):
-    #Returns n-th fibonacci number
-    #Code here
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
 
 def is_prime(n):
-    #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
