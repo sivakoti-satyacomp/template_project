f7f5881b9eec84e8f794594d1c817533ccf2c648
25f2001422 <25f2001422@ds.study.iitm.ac.in>
2026-01-22 08:00:46 +0530

    second function added

--- generate_numbers.py
+++ generate_numbers.py
@@ -4,9 +4,15 @@ def natural_numbers(n):
 
 
 def nth_fibonacci(n):
-    #Returns n-th fibonacci number
-    #Code here
-
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
     #Code here
