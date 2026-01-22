6c1c061b17e07e56a1cbe2a171c86ad2902815f1
NITESH SHARMA <23f3001886@ds.study.iitm.ac.in>
2026-01-22 23:05:45 +0530

    Create prime_natural_fibancco.py

---
+++ prime_natural_fibancco.py
@@ -0,0 +1,21 @@
+def nth_fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+def natural_numbers(n):
+    for i in range(1, n + 1):
+        print(i, end=" ")
+def is_prime(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
