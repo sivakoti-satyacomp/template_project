6bc2aa4be59887cd9bce0ba55d5b09fbc908aa44
24f2001045 <24f2001045@ds.study.iitm.ac.in>
2026-01-23 17:56:57 +0530

    updated with natural nums,fibonacci,prime numbers functions

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,46 @@
 def natural_numbers(n):
-    #Returns the list of natural numbers
-    #Code here
+    numbers = []          
+
+    for i in range(1, n + 1):   
+        numbers.append(i)       
+
+    return numbers         
+
 
 
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
-    #Code here
+    # handle invalid input
+    if n <= 0:
+        return None
+    # first fibonacci number
+    if n == 1:
+        return 0
+    # second fibonacci number
+    if n == 2:
+        return 1
+    # starting values
+    first = 0
+    second = 1
+    # calculate from 3rd to nth number
+    for i in range(3, n + 1):
+        next_value = first + second
+        first = second
+        second = next_value
+
+    return second
+
+print(nth_fibonacci(9))
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    # numbers less than or equal to 1 are not prime
+    if n <= 1:
+        return False
+    # check each number from 2 to n-1
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    # if no number divides n, it is prime
+    return True
