7aa35dde695c732c77312fe4ab63dc07fbbe6419
25f2006383 <25f2006383@dss.study.ac.in>
2026-01-23 10:34:56 +0530

    impleamented naturan number, faibonacci and prime no.

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,32 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
-    #Code here
+    if n <= 0:
+        return []
+    return list(range(1, n + 1))
 
 
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
-    #Code here
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    a, b = 0, 1
+    for _ in range(n - 1):
+        a, b = b, a + b
+    return a
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
--- natural_numbers.py
+++
@@ -1,5 +0,0 @@
-def is_natural_numbers(n):
-    if isinstance(n, int) and n > 0:
-        return True
-    else:
-        return False
