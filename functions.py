f28f418b2a43dd06717802a8e8ae2915d0af3f55
23f3002440 <23f3002440@ds.study.iitm.ac.in>
2026-01-23 11:56:44 +0530

    Add natural_numbers, nth_fibonacci, is_prime functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+def natural_numbers(n):
+    return list(range(1, n+1))
+
+def nth_fibonacci(n):
+    if n <= 0:
+        return []
+    fib = [0, 1]
+    while len(fib) < n:
+        fib.append(fib[-1] + fib[-2])
+    return fib[:n]
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
