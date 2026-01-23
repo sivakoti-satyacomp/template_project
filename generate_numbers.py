54268e07b8a298c687c4753b5ce6495f3b45dbaa
Nimish Shinde <23f2002121@ds.study.iitm.ac.in>
2026-01-23 20:51:29 +0530

    Implemented required math functions - 23f2002121

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,18 @@
 def natural_numbers(n):
+    return list(range(1, n+1))
     #Returns the list of natural numbers
     #Code here
 
-
 def nth_fibonacci(n):
+    a, b = 0, 1
+    for meh in range(n):
+        a, b = b, a + b
+    return a
     #Returns n-th fibonacci number
     #Code here
 
 def is_prime(n):
+    if n <= 1:
+        return False
     #Return true if 'n' is prime, otherwise returns false
     #Code here
