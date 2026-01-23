18d0c2a8b6962cf1c83d1680bb4e0c699f7b8f72
Sayan Shil <officialsayan36@gmail.com>
2026-01-23 14:35:57 +0530

    Added code for the list of n natural numbers

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,6 +1,13 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
     #Code here
+    if not isinstance(n, int):
+        raise TypeError("n must be an integer")
+
+    if n <= 0:
+        return []
+
+    return list(range(1, n + 1))
 
 
 def nth_fibonacci(n):
