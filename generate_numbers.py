fb11983ecd82f3cdb4bc674fac03ade276cd5f0a
Ankit Kumar Prajapati <24f3002643@ds.study.iitm.ac.in>
2026-01-23 18:38:31 +0530

    Updating generate_numebers on branch update_feature

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,15 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
     #Code here
+    print()
 
 
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
     #Code here
+    print()
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
     #Code here
+    print()
