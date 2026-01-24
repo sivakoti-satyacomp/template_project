4d5ee13896ae5b6edf022fa24a7a9ef334608ce7
24f3004766 <24f3004766@ds.study.iitm.ac.in>
2026-01-24 14:29:13 +0000

    new brach added  which have new feature

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,36 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
-    #Code here
+    if n < 1:
+        return []
+    return list(range(1, n + 1))
 
 
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
-    #Code here
+    if n < 0:
+        raise ValueError("n must be a non-negative integer")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0:
+        return False
+
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
