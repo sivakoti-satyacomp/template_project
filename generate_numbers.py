5ea5e0426a5624725b3d5319a021586f937f9988
ArjyaDutta-IITM <131282819+ArjyaDutta-IITM@users.noreply.github.com>
2026-01-26 01:13:56 +0530

    The `THREE` functions: _natural_numbers(n), nth_fibonacci(n)_ and _is_prime(n)_ are implemented.

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,15 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
     #Code here
-
+    return list(range(1, n))
 
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
     #Code here
+    sqrt_5 = 5 ** 0.5
+    return int((((1+sqrt_5)/2)**n-((1-sqrt_5)/2)**n)/sqrt_5)
 
-def is_prime(n):
+def is_prime(n, i=2):
     #Return true if 'n' is prime, otherwise returns false
     #Code here
+    return n >= 2 and (i >= n or bool(n % i) and is_prime(n, i + 1))
