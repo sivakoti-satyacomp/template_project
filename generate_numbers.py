cde6b0569f9a7ebabffcd5a6155880d61dab9a97
21f1002425 <21f1002425@ds.study.iitm.ac.in>
2026-01-25 14:23:45 +0530

    Implemented functions: natural_numbers, fibonacci, is_prime

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,27 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
-    #Code here
-
+    return [i for i in range(1, n + 1)]
 
 def nth_fibonacci(n):
-    #Returns n-th fibonacci number
-    #Code here
+    # Returns the n-th fibonacci number
+    # Sequence: 0, 1, 1, 2, 3, 5... (0-indexed)
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(2, n):
+        a, b = b, a + b
+    return b
 
 def is_prime(n):
-    #Return true if 'n' is prime, otherwise returns false
-    #Code here
+    # Return true if 'n' is prime, otherwise returns false
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
