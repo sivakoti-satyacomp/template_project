464c105f9698a9ab95d399c2d0804580be78887d
25f2006383 <25f2006383@dss.study.ac.in>
2026-01-25 16:07:48 +0530

    Readme changes

--- generate_numbers.py
+++ generate_numbers.py
@@ -2,9 +2,11 @@ def natural_numbers(n):
     #Returns the list of natural numbers
     #Code here
 
+
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
     #Code here
+
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
