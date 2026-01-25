24269a3de794f3f549fab804296af9284c883aac
24f3000679shiva <24f3000679@ds.study.iitm.ac.in>
2026-01-25 23:20:18 +0530

    Update generate_numbers.py
    is_prime

--- generate_numbers.py
+++ generate_numbers.py
@@ -26,4 +26,17 @@ def nth_fibonacci(n):
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
     #Code here
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
     
