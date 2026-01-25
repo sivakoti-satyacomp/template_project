3bf755fd375e78a9ff083c9aaa8dada097ceca6b
24f3000679shiva <24f3000679@ds.study.iitm.ac.in>
2026-01-25 23:19:35 +0530

    Update generate_numbers.py
    fibonacci number

--- generate_numbers.py
+++ generate_numbers.py
@@ -10,6 +10,18 @@ def natural_numbers(n):
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
     #Code here
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(3, n + 1):
+        a, b = b, a + b
+    return b
+
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
