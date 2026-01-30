cd14003150b796bc074789b385986f941822fbfc
21f1006744 <21f1006744@ds.study.iitm.ac.in>
2026-01-30 21:48:58 +0530

    Implemented all  the THREE functions: natural_numbers(n), nth_fibonacci(n) and is_prime(n) by 21f1006744

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,12 +1,30 @@
 def natural_numbers(n):
     #Returns the list of natural numbers
     #Code here
+    result = []
+    for i in range(1, n + 1):
+        result.append(i)
+    return result
 
 
 def nth_fibonacci(n):
     #Returns n-th fibonacci number
     #Code here
+    if n < 0:
+        return None  # or raise an error
+
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
 
 def is_prime(n):
     #Return true if 'n' is prime, otherwise returns false
     #Code here
+    if n < 2:
+        return False
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
